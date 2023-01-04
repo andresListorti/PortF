963b23adc2cff7cb11b628bc3b00276a801adf7b
andresListorti <andreslistorti@gmail.com>
2023-01-04 13:16:31 -0300

    add11

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,23 +1,24 @@
 import React from "react";
-import '../ProjectCard/projectCard.css';
+import "../ProjectCard/projectCard.css";
 
 const ProjectCard = ({ img_src, projectName, projectLink }) => {
-    return (
-        <div className='projectCard-wrapper'>
-            <div className='top'>
-                <img src={img_src} alt={projectName} />
-            </div>
-            <div className='bottom'>
-                <a 
-                    className="project-link"
-                    href={projectLink}
-                    target='_blank'
-                >
-                    {projectName}
-                </a>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="projectCard-wrapper">
+      <div className="top">
+        <img src={img_src} alt={projectName} />
+      </div>
+      <div className="bottom">
+        <a
+          className="project-link"
+          href={projectLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {projectName}
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default ProjectCard;
